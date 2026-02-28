c0bf28e88465d217128fe29ec4f8f2abc840cf0e
dialite <raymondolisa01@gmail.com>
2026-02-28 10:11:53 +0100

    Modify middleware by removing arcject functionality

--- middleware.js
+++ middleware.js
@@ -7,20 +7,7 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY: SEARCH_ENGINE", "GO_HTTP"],
-    }),
-  ],
-});
-
-const clerk = clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
@@ -30,8 +17,6 @@ const clerk = clerkMiddleware(async (auth, req) => {
   }
 });
 
-export default createMiddleware(aj, clerk);
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
@@ -40,3 +25,46 @@ export const config = {
     "/(api|trpc)(.*)",
   ],
 };
+
+// import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher([
+//   "/dashboard(.*)",
+//   "/account(.*)",
+//   "/transaction(.*)",
+// ]);
+
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY,
+//   rules: [
+//     shield({
+//       mode: "LIVE",
+//     }),
+//     detectBot({
+//       mode: "LIVE",
+//       allow: ["CATEGORY: SEARCH_ENGINE", "GO_HTTP"],
+//     }),
+//   ],
+// });
+
+// const clerk = clerkMiddleware(async (auth, req) => {
+//   const { userId } = await auth();
+
+//   if (!userId && isProtectedRoute(req)) {
+//     const { redirectToSignIn } = await auth();
+
+//     return redirectToSignIn();
+//   }
+// });
+
+// export default createMiddleware(aj, clerk);
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
